a544c83add8be3fd56701f4f5df327764877177a
tangxiao <1037706321@qq.com>
2023-02-15 16:58:08 +0800

    Linklsit

--- src/LinkenList/LoopNode.java
+++ src/LinkenList/LoopNode.java
@@ -2,9 +2,27 @@ package LinkenList;
 
 import sun.nio.cs.ext.MacHebrew;
 
+import java.lang.management.ManagementFactory;
+
 public class LoopNode {
 
-    //判断1个链是否有环
+    public static Node getinsersectNode(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        Node loop1 = getLoopNode(head1);
+        Node loop2 = getLoopNode(head2);
+        if (loop1 == null || loop2 == null) {
+            return noloop(head1, head2);
+        }
+        if (loop1 != null & loop2 != null) {
+            return bothLoop(head1, loop1, head2, loop2);
+        }
+        return null;
+
+    }
+
+    //判断1个链是否有环   快慢指针
     public static Node getLoopNode(Node head) {
         if (head == null || head.next == null || head.next.next == null) {
             return null;
@@ -26,6 +44,7 @@ public class LoopNode {
         return fasterPointer;
     }
 
+    //两个没环 求相交
     public static Node noloop(Node head1, Node head2) {
         if (head1 == null || head2 == null) {
             return null;
@@ -59,5 +78,50 @@ public class LoopNode {
             cur2 = cur2.next;
         }
         return cur1;
+    }
+
+    public static Node bothLoop(Node head1, Node loop1, Node head2, Node loop2) {
+        Node cur1;
+        Node cur2;
+        if (loop1 == loop2) {
+            cur1 = head1;
+            cur2 = head2;
+            int n = 0;
+            while (cur1 != loop1) {
+                n++;
+                cur1 = cur1.next;
+            }
+            while (cur2 != loop2) {
+                n--;
+                cur2 = cur2.next;
+            }
+            if (n > 0) {
+                n = Math.abs(n);
+                while (n != 0) {
+                    cur1 = cur1.next;
+                    n--;
+                }
+            } else if (n < 0) {
+                n = Math.abs(n);
+                while (n != 0) {
+                    cur2 = cur2.next;
+                    n--;
+                }
+            }
+            while (cur1 != cur2) {
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+            return cur1;
+        } else {
+            cur1 = loop1.next;
+            while (cur1 != loop1) {
+                if (cur1 == loop2) {
+                    return loop1;
+                }
+                cur1 = cur1.next;
+            }
+            return null;
+        }
     }
 }
--- src/Test/JavaBasic/Gather/Collection.java
+++ src/Test/JavaBasic/Gather/Collection.java
@@ -1,8 +1,7 @@
 package Test.JavaBasic.Gather;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.LinkedList;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Collection {
 
@@ -40,4 +39,58 @@ class NumberComparator implements Comparator<Integer> {
     public int compare(Integer o1, Integer o2) {
         return o1 - o2;
     }
+}
+
+class Queue {
+    public static void main(String[] args) {
+        ArrayBlockingQueue queue = new ArrayBlockingQueue(3);
+    }
+}
+
+class Map {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put("a", "b");
+        System.out.println(map);
+
+
+    }
+}
+
+class Hashtable {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+        map.put("c", 3);
+        //keyset 获取所有键名
+        Set<String> keys = map.keySet();
+        for (String key : keys) {
+            System.out.println(key);
+        }
+
+        //keys.iterator 获取keys 迭代器
+        Iterator<String> iterator = keys.iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            if ("b".equals(key)) {
+                iterator.remove();
+            }
+            System.out.println(key);
+        }
+
+    }
+}
+
+class Utils {
+    public static void main(String[] args) {
+//        Arrays
+        int[] is = {1, 2, 3, 5, 4, 8};
+
+        List<Integer> integers = Arrays.asList(1, 2, 3, 4);
+
+
+
+    }
+
 }
